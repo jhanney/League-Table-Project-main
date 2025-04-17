2905e0df0196771d53984aebab7eb34f7a33e698
JoshuaHanney <163124064+JoshuaHanney@users.noreply.github.com>
2025-04-17 18:31:28 +0100

    update serie-a.html with same code

--- src/app/serie-a-table/serie-a-table.page.ts
+++ src/app/serie-a-table/serie-a-table.page.ts
@@ -1,20 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCard, IonCol, IonCardHeader, IonCardTitle } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCard, IonCol, IonCardHeader, IonCardTitle, IonBackButton, IonButtons, IonCardContent } from '@ionic/angular/standalone';
+import { LeagueService } from '../league.service';
+
 
 @Component({
   selector: 'app-serie-a-table',
   templateUrl: './serie-a-table.page.html',
   styleUrls: ['./serie-a-table.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonGrid, IonRow, IonCard, IonCol, IonCardHeader, IonCardTitle]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonGrid, IonRow, IonCard, IonCol, IonCardHeader, IonCardTitle, IonBackButton, IonButtons, IonCardContent]
 })
 export class SerieATablePage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  standings: any[] = []; 
+    isLoading: boolean = true; //debugging incase page is not loading
+    errorMessage: string = ''; //error message to
+  
+    constructor(private leagueService: LeagueService) { }
+  
+    
+    ngOnInit() {
+      this.loadStandings(); //load the table
+    }
+  
+    loadStandings(){
+      this.isLoading = true; //set to true
+      this.errorMessage = ''; 
+  
+      //get standings for PL
+      this.leagueService.getStandings('SA').subscribe({
+        next: (response) => {
+          console.log('Response from API', response); //log API response for debug
+  
+          //making sure the data is fecthed
+          if(response && response.standings && response.standings.length > 0){
+            this.standings = response.standings[0].table;//access the table
+          }else {
+            this.errorMessage = 'No standings available'; 
+          }
+          this.isLoading = false; 
+        }, 
+        error: (error) =>{
+          this.errorMessage = 'Failed to load standings, theres been an error'; 
+          this.isLoading = false; 
+        }
+      });
+    }
 
 }
